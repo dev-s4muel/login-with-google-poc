b7d56dfbdb803a29638dea4b6c45cee30c391f76
sgmartinss <sgmartins777@gmail.com>
2025-03-15 21:10:53 -0300

    first commit

---
+++ src/main/java/com/poc/login_with_google_poc/LoginWithGooglePocApplication.java
@@ -0,0 +1,13 @@
+package com.poc.login_with_google_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginWithGooglePocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginWithGooglePocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/login_with_google_poc/LoginWithGooglePocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.login_with_google_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginWithGooglePocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
