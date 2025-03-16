21048046bcd7cf1e659354fa81bd3d95a258324d
sgmts <sgmartins777@gmail.com>
2025-03-16 16:27:40 -0300

    implementado Login com o google

--- src/main/java/com/poc/login_with_google_poc/LoginWithGooglePocApplication.java
+++ src/main/java/com/poc/login_with_google_poc/LoginWithGooglePocApplication.java
@@ -2,8 +2,10 @@ package com.poc.login_with_google_poc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class LoginWithGooglePocApplication {
 
 	public static void main(String[] args) {
